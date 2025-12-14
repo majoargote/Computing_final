8ad6d8a6fcb79040860191080024e16027e4248b
majoargote <majoargote@gmail.com>
2025-12-14 13:27:28 +0100

    tests for preprocessing and fixed scalers and init py files

--- wine_predict/preprocessing/__init__.py
+++ wine_predict/preprocessing/__init__.py
@@ -2,10 +2,9 @@
 # Preprocessing module for wine quality prediction
 
 from .base import BasePreprocessor
-from .scalers import StandardScalerPreprocessor, RobustScalerPreprocessor
+from .scalers import StandardScalerPreprocessor
 
 __all__ = [
     'BasePreprocessor',
-    'StandardScalerPreprocessor',
-    'RobustScalerPreprocessor'
+    'StandardScalerPreprocessor'
 ]
--- wine_predict/preprocessing/scalers.py
+++ wine_predict/preprocessing/scalers.py
@@ -1,6 +1,6 @@
-from sklearn.preprocessing import StandardScaler, RobustScaler
+from sklearn.preprocessing import StandardScaler
 import pandas as pd
-from .base import BasePreprocessor
+from wine_predict.preprocessing.base import BasePreprocessor
 
 
 class StandardScalerPreprocessor:
@@ -11,27 +11,6 @@ class StandardScalerPreprocessor:
     
     def fit(self, X, y=None):
 
-        self.scaler.fit(X)
-        self.feature_names = X.columns.tolist()
-        self.is_fitted = True
-        return self
-    
-    def transform(self, X):
-        X_scaled = self.scaler.transform(X)
-        return pd.DataFrame(
-            X_scaled,
-            columns=self.feature_names,
-            index=X.index
-        )
-
-
-class RobustScalerPreprocessor(BasePreprocessor):
-    def __init__(self):
-        super().__init__()
-        self.scaler = RobustScaler()
-        self.feature_names = None
-    
-    def fit(self, X, y=None):
         self.scaler.fit(X)
         self.feature_names = X.columns.tolist()
         self.is_fitted = True
